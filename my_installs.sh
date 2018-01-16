eb22ebc7a1280a723221d820f892fa67a4ced7fe
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2018-01-16 17:28:58 -0600

    Se añadio las notificaciones a i3

--- my_installs.sh
+++ my_installs.sh
@@ -8,7 +8,7 @@ neofetch
 
 # Install programs
 sudo pacman -Syu
-sudo pacman -S --needed atom compton dropbox gparted i3-gaps i3blocks i3lock i3status dmenu gimp libnotify npm rofi telegram-desktop wireshark-qt yaourt xf86-video-intel xf86-video-nouveau
+sudo pacman -S --needed atom compton dropbox gparted i3-gaps i3blocks i3lock i3status dmenu gimp libnotify npm rofi spotify telegram-desktop wireshark-qt yaourt xf86-video-intel xf86-video-nouveau
 yaourt -S --needed neofetch twmn-git
 
 read -p "Execute npm install? (y/n) " -n 1 -r
@@ -19,6 +19,9 @@ then
 fi
 
 cp ./i3_config /home/$USER/.config/i3/config
+
+# twmnd
+# notify-send 'It Works!' 'This is an example notification.' --icon=dialog-information
 
 read -p "Reboot? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
