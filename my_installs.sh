e73af417784d50d7a3eafd79a4d6d14436dc4771
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2018-01-17 00:24:21 -0600

    Se añadio spotify

--- my_installs.sh
+++ my_installs.sh
@@ -8,8 +8,8 @@ neofetch
 
 # Install programs
 sudo pacman -Syu
-sudo pacman -S --needed atom compton dropbox gparted i3-gaps i3blocks i3lock i3status dmenu gimp libnotify npm rofi spotify telegram-desktop wireshark-qt yaourt xf86-video-intel xf86-video-nouveau
-yaourt -S --needed neofetch twmn-git
+sudo pacman -S --needed atom compton dropbox gparted i3-gaps i3blocks i3lock i3status dmenu gimp libnotify npm rofi telegram-desktop wireshark-qt yaourt xf86-video-intel xf86-video-nouveau
+yaourt -S --needed neofetch twmn-git spotify
 
 read -p "Execute npm install? (y/n) " -n 1 -r
 echo
