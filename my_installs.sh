6a5163e14ce70f800fcd0e9b68542f855508088f
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2018-01-17 18:51:18 -0600

    Se añadio cmatrix

--- my_installs.sh
+++ my_installs.sh
@@ -8,7 +8,7 @@ neofetch
 
 # Install programs
 sudo pacman -Syu
-sudo pacman -S --needed atom compton dropbox gparted i3-gaps i3blocks i3lock i3status dmenu gimp libnotify npm rofi telegram-desktop wireshark-qt yaourt xf86-video-intel xf86-video-nouveau
+sudo pacman -S --needed atom cmatrix compton dropbox gparted i3-gaps i3blocks i3lock i3status dmenu gimp libnotify npm rofi telegram-desktop wireshark-qt yaourt xf86-video-intel xf86-video-nouveau
 yaourt -S --needed neofetch twmn-git musixmatch-bin spotify
 
 read -p "Execute npm install? (y/n) " -n 1 -r
@@ -18,22 +18,27 @@ then
 	sudo npm install gtop -g # --needed flag? :( this need a fix
 fi
 
-cp ./i3_config /home/$USER/.config/i3/config
+read -p "Copy i3_config? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	cp ./i3_config /home/$USER/.config/i3/config
+fi
 
 # twmnd
 # notify-send 'It Works!' 'This is an example notification.' --icon=dialog-information
 
 read -p "Reboot? (y/n) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo
-	echo
-	echo "See you space cowboy ..."
-	echo
-	sudo reboot
-fi
 
 echo
 echo
 echo "See you space cowboy ..."
 echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo reboot
+fi
+
+#cmatrix &
+
