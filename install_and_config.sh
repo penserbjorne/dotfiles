d696d9601f1c6e5f771cd0361561a130b45e4db1
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2018-08-11 11:12:40 -0500

    Se cambio nombre y se reacomodo un poco el archivo

---
+++ install_and_config.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# author: Penserbjorne
+# description: Script to install my usual software and manage some configurations in my PC's with Arch
+
+# Show system info
+neofetch
+
+# Update system
+sudo pacman -Syudd --force --needed
+
+# Install i3 related stuff
+sudo pacman -S --needed compton i3-gaps i3blocks i3lock i3status dmenu libnotify rofi
+yaourt -S --force --needed --noconfirm twmn-git polybar-git
+
+# Install programs
+sudo pacman -S --needed atom dropbox gparted iptables keepassxc gimp nftables npm \
+	telegram-desktop wireshark-qt yaourt xf86-video-intel xf86-video-nouveau
+yaourt -S --force --needed --noconfirm neofetch musixmatch-bin spotify signal-desktop-bin
+
+# Install funny stuffs
+sudo pacman -S --needed --noconfirm cmatrix
+
+read -p "Install Java10/JDK10 related? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo pacman -S --needed jre10-openjdk-headless jre10-openjdk jdk10-openjdk openjdk10-doc openjdk10-src
+fi
+
+read -p "Install gtop? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo npm install gtop -g # --needed flag? :( this need a fix
+fi
+
+read -p "Copy i3_config? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	cp ./i3_config /home/$USER/.config/i3/config
+fi
+
+read -p "Reboot? (y/n) " -n 1 -r
+
+echo
+echo
+echo "See you space cowboy ..."
+echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo reboot
+fi
+
+#cmatrix &
